f4b4cf523ef0f59f87559b59523b6f4636f954f5
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-04-17 17:30:11 -0500

    0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+check if all lockboxes can be access
+"""
+
+
+def canUnlockAll(boxes):
+    visited = []
+    for i in range(len(boxes)):
+        for j in range(len(boxes[i])):
+            if boxes[i][j] > i:
+                visited.append(boxes[i][j])
+
+    if max(visited) == len(boxes) - 1:
+        return True
+    else:
+        return False
